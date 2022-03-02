66ccdaabd497a4f3a38811b2b2c93f44955d4faa
minetaro12 <osawa2003@gmail.com>
2022-03-02 10:07:59 +0900

    AAC AVC指定ができるように

--- src/download.ts
+++ src/download.ts
@@ -11,19 +11,40 @@ router.get('/', (req, res) => {
     res.send('GET /download?url=videoid');
   } else {
     const videoID = `${req.query.url}`;
+    const avcOp = `${req.query.avc}`;
+    const aacOp = `${req.query.aac}`;
     const filename = str.random(30); //ランダムなファイル名を定義
     const videoFile = `tmp/${filename}.mp4`;
     const audioFile = `tmp/${filename}.m4a`;
     const outFile = `tmp/${filename}_out.mp4`;
     console.log(`Start ${videoID}`);
+    console.log(`avc: ${avcOp}`);
+    console.log(`aac: ${aacOp}`);
     (async () => {
       try {
         const title = (await ytdl.getInfo(videoID)).videoDetails.title; //タイトルを取得
         res.set({'Content-Disposition': `attachment; filename=${encodeURIComponent(title)}.mp4`});
-        ytdl(videoID, {quality: 'highestvideo'}).pipe(fs.createWriteStream(videoFile)) //動画ファイルを取得
+
+        let dlVformat: ytdl.downloadOptions | undefined;
+        if(avcOp == '1') { //AVC指定
+          // @ts-expect-error
+          dlVformat = {quality: 'highestvideo', filter: format => format.videoCodec?.startsWith('avc1')};
+        } else {
+          dlVformat = {quality: 'highestvideo'};
+        };
+
+        let dlAformat: ytdl.downloadOptions | undefined;
+        if(aacOp == '1') { //AAC指定
+          // @ts-expect-error
+          dlAformat = {quality: 'highestaudio', filter: format => format.audioCodec?.startsWith('mp4a')};
+        } else {
+          dlAformat = {quality: 'highestaudio'};
+        };
+
+        ytdl(videoID, dlVformat).pipe(fs.createWriteStream(videoFile)) //動画ファイルを取得
           .on('finish', () => {
             console.log(`${videoID} => ${videoFile}`);
-            ytdl(videoID, {quality: 'highestaudio'}).pipe(fs.createWriteStream(audioFile)) //音声ファイルを取得
+            ytdl(videoID, dlAformat).pipe(fs.createWriteStream(audioFile)) //音声ファイルを取得
               .on('finish', () => {
                 console.log(`${videoID} => ${audioFile}`);
                 ffmpeg() //動画と音声をマージ
