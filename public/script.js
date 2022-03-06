cb0b843a9668efa77fdd69c7e26ab398d7fe89a2
minetaro12 <osawa2003@gmail.com>
2022-03-06 18:21:13 +0900

    ダウンロード時の見た目を変更

--- public/script.js
+++ public/script.js
@@ -1,11 +1,27 @@
 let mp3Mode;
 let avcQuery;
 let aacQuery;
-const statusText = document.getElementById('status');
+
+const submitButton = document.getElementById('submitButton');
+const statusArea = document.getElementById('statusArea');
 const urlText = document.getElementById('downloadUrl');
 const avcCheck = document.getElementById('avcOption');
 const aacCheck = document.getElementById('aacOption');
 const mp3Check = document.getElementById('mp3Option');
+
+const waitHtml = '<button class="btn btn-primary" type="button" disabled>\
+  <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>\
+  Please Wait...\
+  </button>';
+
+const dlHtml = '<button class="btn btn-primary" type="button" disabled>\
+  <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>\
+  Downloading...\
+  </button>';
+
+const defHtml = '<input type="button" value="おとす" onclick="submit()" class="btn btn-outline-primary">';
+
+const errHtml = '<div class="alert alert-danger" role="alert">エラーが発生しました</div>';
 
 window.onload = function() {
   mp3Mode = false;
@@ -30,9 +46,11 @@ const valueChange = function() {
 
 //ダウンロード処理
 const submit = function() {
+  statusArea.innerHTML = '';
+
   //何も入力されていない場合
   if(!urlText.value) {
-    alert('URLを入力してください');
+    statusArea.innerHTML = '<div class="alert alert-warning" role="alert">URLを入力してください</div>';
     return;
   };
 
@@ -52,13 +70,13 @@ const submit = function() {
 
   let fileName;
   if(mp3Mode == true) {
-    statusText.innerHTML = '<p>Please Wait...<img src="https://www.benricho.org/loading_images/img-transparent/712-24.gif"></p>';
+    submitButton.innerHTML = waitHtml;
     fetch(`/mp3?url=${videoId}`)
     .then((res) => {
       if (!res.ok) {
         throw new Error;
       };
-      statusText.innerHTML = '<p>Downloading...<img src="https://www.benricho.org/loading_images/img-transparent/712-24.gif"></p>';
+      submitButton.innerHTML = dlHtml;
       const header = res.headers.get('Content-Disposition');
       const parts = header.split(';');
       fileName = decodeURIComponent(parts[1].split('=')[1]);
@@ -66,7 +84,7 @@ const submit = function() {
       return res.blob();
     })
     .then((data) => {
-      statusText.innerHTML = '<p>Complete!</p>';
+      submitButton.innerHTML = defHtml;
       const a = document.createElement("a");
       a.href = window.URL.createObjectURL(data);
       a.download = fileName;
@@ -74,19 +92,20 @@ const submit = function() {
       a.remove();
     })
     .catch((error) => {
-      statusText.innerHTML = '<p>Error</p>';
+      submitButton.innerHTML = defHtml;
+      statusArea.innerHTML = errHtml;
       return;
     });
   };
 
   if(mp3Mode == false) {
-    statusText.innerHTML = '<p>Please Wait...<img src="https://www.benricho.org/loading_images/img-transparent/712-24.gif"></p>';
+    submitButton.innerHTML = waitHtml;
     fetch(`/download?url=${videoId}${avcQuery}${aacQuery}`)
     .then((res) => {
       if (!res.ok) {
         throw new Error;
       };
-      statusText.innerHTML = '<p>Downloading...<img src="https://www.benricho.org/loading_images/img-transparent/712-24.gif"></p>';
+      submitButton.innerHTML = dlHtml;
       const header = res.headers.get('Content-Disposition');
       const parts = header.split(';');
       fileName = decodeURIComponent(parts[1].split('=')[1]);
@@ -94,7 +113,7 @@ const submit = function() {
       return res.blob();
     })
     .then((data) => {
-      statusText.innerHTML = '<p>Complete!</p>';
+      submitButton.innerHTML = defHtml;
       const a = document.createElement("a");
       a.href = window.URL.createObjectURL(data);
       a.download = fileName;
@@ -102,7 +121,8 @@ const submit = function() {
       a.remove();
     })
     .catch((error) => {
-      statusText.innerHTML = '<p>Error</p>';
+      submitButton.innerHTML = defHtml;
+      statusArea.innerHTML = errHtml;
       return;
     });
   };
